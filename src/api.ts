ff63355dedccc60f27776e95078c1b3b60900c05
Jannik Feuerhahn <jannik@feuer.dev>
2021-02-16 00:31:40 +0800

    removed all semicolons

--- src/api.ts
+++ src/api.ts
@@ -12,21 +12,24 @@ import tarkov from "./tarkov"
 import ddns from "./ddns"
 
 //Constants
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 //Init Webserver
+const app = express()
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
 app.use(cors)
 
 //Start Webserver
 app.listen(PORT, function() {
-  console.log("API running on Port:" + PORT);
-});
+  console.log("API running on Port:" + PORT)
+})
 
 //Handle Routes
 app.use(tarkov)
 app.use(ddns)
 
 app.get("/", function (req, res) {
-  console.log("Get request on / : " + req);
-  res.send("API online");
-});
+  console.log("Get request on / : " + req)
+  res.send("API online")
+})
--- src/ddns.ts
+++ src/ddns.ts
@@ -2,9 +2,9 @@ import express from "express"
 import fs from "fs"
 import path from "path"
 
-const FOLDER_OUT = path.join(__dirname, "../out/");
-const FOLDER_OUT_DDNS = path.join(FOLDER_OUT, "ddns/");
-const FILE_OUT_DDNS_TXT = "ddns.txt";
+const FOLDER_OUT = path.join(__dirname, "../out/")
+const FOLDER_OUT_DDNS = path.join(FOLDER_OUT, "ddns/")
+const FILE_OUT_DDNS_TXT = "ddns.txt"
 
 const router = express.Router()
 
@@ -20,29 +20,29 @@ router.get("/update", function (req, res) {
     if(key && domain && user && ip) {
       //check if values are valid
       if(user === process.env.API_USER && key === process.env.API_KEY && domain == process.env.API_DDNS_DOMAIN) {
-        console.log("Valid DDNS update request from user: " + user);
+        console.log("Valid DDNS update request from user: " + user)
         //Write IP to /out/
         fs.mkdir(FOLDER_OUT, function (err) {
           if(err) {
-            console.log(err);
+            console.log(err)
           }
           fs.mkdir(FOLDER_OUT_DDNS, function (err) {
             if(err) {
-              console.log(err);
+              console.log(err)
             }
             fs.writeFile(FOLDER_OUT_DDNS + FILE_OUT_DDNS_TXT, ip, function (err) {
               if(err) {
-                console.log(err);
+                console.log(err)
               }
-              console.log("Successfully set new IP to: " + ip);
-              console.log("Update has two minutes TTL");
-              res.status(202).send();
-            });
-          });
-        });
-      } else res.status(403).send();
-    } else res.status(400).send();
-  } else res.status(400).send();
-});
+              console.log("Successfully set new IP to: " + ip)
+              console.log("Update has two minutes TTL")
+              res.status(202).send()
+            })
+          })
+        })
+      } else res.status(403).send()
+    } else res.status(400).send()
+  } else res.status(400).send()
+})
 
 export default router
